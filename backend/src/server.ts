c03fd31b8ca33daf85b038d1f41d2f6308999e80
Bhumipat Kusalatham <74818019+OverCatX@users.noreply.github.com>
2025-09-17 19:05:49 +0700

    fix(backend): fix bug

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -6,7 +6,7 @@ dotenv.config();
 
 const PORT = process.env.PORT || 5000;
 
-mongoose.connect(process.env.MONGO_UR as string)
+mongoose.connect(process.env.MONGO_URL as string)
   .then(() => {
     console.log("MongoDB connected");
     app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
