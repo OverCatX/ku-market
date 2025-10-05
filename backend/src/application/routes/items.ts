456da6899f1f96d9922cb197daded1a8cec12651
Thapanan Suwansukhum <thapanansu555@gmail.com>
2025-10-05 22:36:02 +0700

    refactor(backend): add authentication for item route

--- backend/src/application/routes/items.ts
+++ backend/src/application/routes/items.ts
@@ -2,15 +2,16 @@ import express from "express";
 import { uploadFiles } from "../middlewares/validators/items.validation";
 import ItemController from "../controllers/items.controller";
 import { createItem, updateItem} from "../middlewares/validators/items.validation";
+import { authenticate } from "../middlewares/authentication";
 
 const router = express.Router();
 const itemcontroller = new ItemController();
 
-router.post("/create",uploadFiles("photos", 5),createItem, itemcontroller.userUpload);
+router.post("/create",authenticate ,uploadFiles("photos", 5, true),createItem, itemcontroller.userUpload);
 
-router.patch("/update/:id", uploadFiles("photos", 5),updateItem, itemcontroller.userUpdatePicture);
+router.patch("/update/:id",authenticate,  uploadFiles("photos", 5, false),updateItem, itemcontroller.userUpdatePicture);
 
-router.delete("/delete/:id", itemcontroller.userDeletePicture)
+router.delete("/delete/:id",authenticate, itemcontroller.userDeletePicture)
 
 router.get("/list", itemcontroller.userGetList)
 
