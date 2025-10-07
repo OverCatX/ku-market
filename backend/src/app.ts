9673b1da3b4ff7bb82a6ebc5aed8bced0cf14e5f
Bhumipat Kusalatham <overcatpocket@gmail.com>
2025-10-07 17:18:38 +0700

    feat(backend): enhance app setup with TypeScript support and body-parser integration

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -1,11 +1,13 @@
-import express from "express";
+import express, { Application } from "express";
 import cors from "cors";
+import bodyParser from "body-parser";
 import authRoutes from "./application/routes/auth";
 import profileRoutes from "./application/routes/profile";
 import itemRoutes from "./application/routes/items";
 
-const app = express();
+const app: Application = express();
 app.use(cors());
+app.use(bodyParser.json());
 app.use(express.json());
 
 app.use("/api/auth", authRoutes);
