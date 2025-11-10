f953f2f50e3a3588b2e736647d8defd8c10ab59e
Thapanan Suwansukhum <thapanansu555@gmail.com>
2025-11-11 00:02:40 +0700

    refactor(backend): move Stripe initialization inside createCheckoutSession method

--- backend/src/application/controllers/checkout.controller.ts
+++ backend/src/application/controllers/checkout.controller.ts
@@ -1,8 +1,6 @@
 import { Stripe } from 'stripe';
 import { Request, Response } from 'express';
 
-const stripe = new Stripe(process.env.STRIPE_SECRET_KEY!);
-
 interface Item {
   name: string;
   price: number;
@@ -12,22 +10,23 @@ interface Item {
 export class CheckoutController {
   createCheckoutSession = async (req: Request, res: Response): Promise<Response> => {
     try {
-      const { items } = req.body as { items: Item[] };
+        const stripe = new Stripe(process.env.STRIPE_SECRET_KEY!);
+        const { items } = req.body as { items: Item[] };
 
-      if (!items || !Array.isArray(items) || items.length === 0) {
-        return res.status(400).json({ error: "Items array is required" });
-      }
+        if (!items || !Array.isArray(items) || items.length === 0) {
+            return res.status(400).json({ error: "Items array is required" });
+        }
 
-      const line_items: Stripe.Checkout.SessionCreateParams.LineItem[] = items.map((item) => ({
-        price_data: {
-          currency: 'thb',
-          product_data: {
-            name: item.name,
-          },
-          unit_amount: item.price * 100,
-        },
-        quantity: item.quantity,
-      }));
+        const line_items: Stripe.Checkout.SessionCreateParams.LineItem[] = items.map((item) => ({
+            price_data: {
+            currency: 'thb',
+            product_data: {
+                name: item.name,
+            },
+            unit_amount: item.price * 100,
+            },
+            quantity: item.quantity,
+        }));
 
         const session = await stripe.checkout.sessions.create({
         payment_method_types: ['card', 'promptpay'],
@@ -40,16 +39,17 @@ export class CheckoutController {
         });
 
 
-      if (!session.url) {
-        return res.status(500).json({ error: "Failed to create session URL" });
-      }
+        if (!session.url) {
+            return res.status(500).json({ error: "Failed to create session URL" });
+        }
 
-      return res.status(200).json({ url: session.url });
+        return res.status(200).json({ url: session.url });
+
     } catch (err: unknown) {
-      if (err instanceof Error) {
-        return res.status(500).json({ error: err.message });
-      }
-      return res.status(500).json({ error: 'Unknown error' });
+        if (err instanceof Error) {
+            return res.status(500).json({ error: err.message });
+        }
+        return res.status(500).json({ error: 'Unknown error' });
     }
   };
 }
