09b703c745e12ae8c31f23608e09494d34669975
Thapanan Suwansukhum <thapanansu555@gmail.com>
2025-11-10 23:53:48 +0700

    feat(backend): implement CheckoutController for Stripe payment processing

---
+++ backend/src/application/controllers/checkout.controller.ts
@@ -0,0 +1,59 @@
+import { Stripe } from 'stripe';
+import { Request, Response } from 'express';
+
+if (!process.env.STRIPE_SECRET_KEY) {
+  throw new Error("STRIPE_SECRET_KEY is not defined in .env");
+}
+
+const stripe = new Stripe(process.env.STRIPE_SECRET_KEY!);
+
+interface Item {
+  name: string;
+  price: number;
+  quantity: number;
+}
+
+export class CheckoutController {
+  createCheckoutSession = async (req: Request, res: Response): Promise<Response> => {
+    try {
+      const { items } = req.body as { items: Item[] };
+
+      if (!items || !Array.isArray(items) || items.length === 0) {
+        return res.status(400).json({ error: "Items array is required" });
+      }
+
+      const line_items: Stripe.Checkout.SessionCreateParams.LineItem[] = items.map((item) => ({
+        price_data: {
+          currency: 'thb',
+          product_data: {
+            name: item.name,
+          },
+          unit_amount: item.price * 100,
+        },
+        quantity: item.quantity,
+      }));
+
+        const session = await stripe.checkout.sessions.create({
+        payment_method_types: ['card', 'promptpay'],
+        mode: 'payment',
+        line_items,
+        success_url: 'http://localhost:3000/success?session_id={CHECKOUT_SESSION_ID}',
+        cancel_url: 'http://localhost:3000/cancel',
+        locale: 'th',
+        submit_type: 'pay',
+        });
+
+
+      if (!session.url) {
+        return res.status(500).json({ error: "Failed to create session URL" });
+      }
+
+      return res.status(200).json({ url: session.url });
+    } catch (err: unknown) {
+      if (err instanceof Error) {
+        return res.status(500).json({ error: err.message });
+      }
+      return res.status(500).json({ error: 'Unknown error' });
+    }
+  };
+}
